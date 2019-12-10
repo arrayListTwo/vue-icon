9062e9b38fb687fa571c057d05757fce5ef5ad73
arrayList <1348909544@qq.com>
2019-12-10 17:27:18 +0800

    使用阿里巴巴图库symbol方式初步实现多色图标加载

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <div class="back">
+      <svg class="icon icon-back" aria-hidden="true">
+        <use xlink:href="#icon-back"></use>
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -8,3 +13,21 @@
     name: 'home'
   }
 </script>
+
+<style lang="stylus" scoped>
+  .home
+    .back
+      .icon
+        width: 1em
+        height: 1em
+        vertical-align: -0.15em
+        fill: rgba(0, 0, 0, 0)
+        overflow: hidden
+
+      .icon-back
+        width 30px
+        height 30px
+
+        &:active, &:focus
+          fill #8CF6FB
+</style>
